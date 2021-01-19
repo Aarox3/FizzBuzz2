6fc220f8daf3fb9eb0cbec236f5d870dc41ac852
Aarox3 <poiuytrewq1990@o2.pl>
2021-01-19 22:16:23 +0100

    Corrected conditios of game.

---
+++ Fizzbuzz/Fizzbuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz
+{
+    public class FizzBuzz
+    {
+        public void AnalyzeNumber(int userNumber)
+        {
+
+            if (userNumber % 5 == 0 && userNumber % 3 == 0)
+            {
+
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (userNumber % 3 == 0)
+            {
+
+                Console.WriteLine("Fizz");
+            }
+            else if (userNumber % 5 == 0)
+            {
+
+                Console.WriteLine("Buzz");
+            }
+
+            else
+            {
+                Console.WriteLine(userNumber);
+            }
+        }
+
+    }
+}
+
---
+++ Fizzbuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz
+{ 
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+
+                try
+                {
+                    Console.WriteLine("Zagrajmy w grę. Grę o nazwie FizzBuzz. Wpisz proszę dowolną liczbę");
+
+                    var userNumber = int.Parse(Console.ReadLine());
+
+                    var fizzbuzz = new FizzBuzz();
+
+                    fizzbuzz.AnalyzeNumber(userNumber);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Podana wartość jest nieprawidłowa, spróbuj jeszcze raz.");
+                }
+            }
+        }
+    }
+}
+
